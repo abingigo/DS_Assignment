19621e529e98408b95ad1de5d5fb6507d2987ba6
abingigo <62868336+abingigo@users.noreply.github.com>
2020-12-01 12:55:03 +0530

    Add files via upload

--- DS_Assignment_1.py
+++ DS_Assignment_1.py
@@ -1,10 +1,7 @@
 #DIsclaimer: This code works in JDoodle, but only on Python 3 and not Python 2
 
 #Function that returns probability of n people having the same birthday in 365 days
-import decimal
-
 def prob(n):
-    decimal.getcontext().prec = 5000
     if n > 365:
         return 1
     c = 1
